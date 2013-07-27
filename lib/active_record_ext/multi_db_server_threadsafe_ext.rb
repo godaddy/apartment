1a0be85d2ef0dfa1d3d8daf6beaa0e556c6bd41d
Yan Zhao <zhaoyan1117@gmail.com>
2013-07-26 20:10:53 -0700

    Fixed connection cannot close bug

--- lib/active_record_ext/multi_db_server_threadsafe_ext.rb
+++ lib/active_record_ext/multi_db_server_threadsafe_ext.rb
@@ -1,4 +1,3 @@
-
 # Original file path:
 # activerecord/lib/active_record/connection_adapters/abstract/connection_pool.rb
 
@@ -9,111 +8,95 @@ if defined?(ActiveRecord)
       class ConnectionHandler
 
         @@debug = false
-        @@thread_first = true
+        @@sleep_time = 0.2
 
         def establish_connection(name, spec)
-
           thread_id = Thread.current.object_id
-          puts "Thread# in establish_connection: #{thread_id}" if @@debug
-  
-          puts "Spec.config in establish_connection is: " if @@debug
-          puts "#{spec.config}" if @@debug
-          
-          @connection_pools[spec] ||= ConnectionAdapters::ConnectionPool.new(spec)
-  
-          if @@thread_first
-            unless @class_to_pool[thread_id] == nil
-              @class_to_pool[thread_id][name] = @connection_pools[spec]
-            else
-              @class_to_pool[thread_id] = {name => @connection_pools[spec]} 
-            end
-          else
-            unless @class_to_pool[name] == nil
-              @class_to_pool[name][thread_id] = @connection_pools[spec]
-            else
-              @class_to_pool[name] = {thread_id => @connection_pools[spec]}
-            end
-          end
 
+          puts "Thread# in establish_connection: #{thread_id}" if @@debug
+          puts "Spec.config in establish_connection is: " #if @@debug
+          puts "#{spec.config}" #if @@debug
+
+          @connection_pools[spec.config] = @connection_pools[spec.config] || ConnectionAdapters::ConnectionPool.new(spec)
+
+          # Generate an array with first value being the thread id, and second value being time_stamp.
+          key = [thread_id, time_stamp]
+
+          unless @class_to_pool[name] == nil
+            @class_to_pool[name][key] = @connection_pools[spec.config]
+          else
+            @class_to_pool[name] = {key => @connection_pools[spec.config]}
+          end
+        rescue RuntimeError => e
+          # If catch an hash iteration error, pause for @@sleep_time seconds.
+          if e.message == "can't add a new key into hash during iteration"
+            sleep @@sleep_time
+            puts "[RESCUE] Catch error: #{e.message}"
+            self.establish_connection(name, spec)
+          else
+            raise e
+          end
         end
   
         def remove_connection(klass)
 
           thread_id = Thread.current.object_id
           puts "Thread# in remove_connection is #{thread_id}" if @@debug
-  
-          if @@thread_first
-            unless @class_to_pool[thread_id] == nil 
-            
-              pool = @class_to_pool[thread_id].delete(klass.name)
-              return nil unless pool
-            
-              used_by_other_threads = false
-              # Check if pool exist in other thread's klass_pool_map.
-              @class_to_pool.each_value do |klass_pool_map|
-                if klass_pool_map.has_value? pool
-                  used_by_other_threads = true
+
+          unless @class_to_pool[klass.name] == nil
+
+            # Iterate through each key, value pair of this class's value (key => coon)
+            # If this coon has current thread as first part of its key, remove this coon.
+            # If not, but this coon is established more than 10 seconds ago, remove this coon. * So for sure this thread is not currently being used.
+            @class_to_pool[klass.name].each do |thread_time, conn|
+              if thread_time[0] == thread_id
+                pool = @class_to_pool[klass.name].delete(thread_time)
+                removed_config = pool.spec.config
+                remove_conn pool
+              else
+                if (Time.now.to_i-thread_time[1]) > 10
+                  pool = @class_to_pool[klass.name].delete(thread_time)
+                  remove_conn pool
                 end
               end
-            
-              unless used_by_other_threads
-                puts "[REMOVE] @class_to_pool[#{thread_id}][#{klass.name}]" if @@debug
-                @connection_pools.delete pool.spec
-                pool.automatic_reconnect = false
-                pool.disconnect!
-              end
-              
-              # Return the config should be done in both cases.
-              pool.spec.config
-            end
-          else
-
-            unless @class_to_pool[klass.name] == nil
-  
-              pool = @class_to_pool[klass.name].delete(thread_id)
-              return nil if pool==nil
-  
-              if @class_to_pool[klass.name].empty?
-                puts "[REMOVE] @class_to_pool[#{klass.name}][#{thread_id}]" if @@debug
-                @connection_pools.delete pool.spec
-                pool.automatic_reconnect = false
-                pool.disconnect!
-              end
-  
-              # Return the config should be done in both cases.
-              pool.spec.config
             end
 
+            # Return the config should be done in both cases.
+            removed_config if defined? (removed_config)
           end
         end
-  
+
+        # Delete current coon from connection_pools and disconnect it.  
+        def remove_conn(pool)
+          puts "[REMOVE] Remove connection: #{pool.spec.config}" if @@debug
+          @connection_pools.delete pool.spec.config
+          pool.automatic_reconnect = false
+          pool.disconnect!
+        end
+
         def retrieve_connection_pool(klass)
           thread_id = Thread.current.object_id
           puts "Thread# in retrive_conenction_pool #{thread_id}" if @@debug
 
-          if @@thread_first
-            pool = @class_to_pool[thread_id][klass.name]
-            
-            return pool if pool
+          unless @class_to_pool[klass.name].nil?
+            pool = nil
+
+            @class_to_pool[klass.name].each do |thread_time, coon|
+              if thread_time[0] == thread_id
+                pool = coon
+                break
+              end
+            end 
+
+            return pool if pool != nil
             return nil if ActiveRecord::Base == klass
-            
+
             # Even if the it goes to the superclass, 
             #   the logic will only look in the corresponding thread_id.
             retrieve_connection_pool klass.superclass
           else
-            unless @class_to_pool[klass.name].nil?
-              pool = @class_to_pool[klass.name][thread_id]
-  
-              return pool if pool != nil
-              return nil if ActiveRecord::Base == klass
-  
-              # Even if the it goes to the superclass, 
-              #   the logic will only look in the corresponding thread_id.
-              retrieve_connection_pool klass.superclass
-            else
-              return nil if ActiveRecord::Base == klass
-              retrieve_connection_pool klass.superclass
-            end
+            return nil if ActiveRecord::Base == klass
+            retrieve_connection_pool klass.superclass
           end
 
         rescue Exception => e
@@ -123,6 +106,11 @@ if defined?(ActiveRecord)
           puts "----------------------------"
           puts "klass name is #{klass.name}"
           raise e
+        end
+
+        # Return the unix time stamp.
+        def time_stamp
+          Time.now.to_i
         end
 
       end
